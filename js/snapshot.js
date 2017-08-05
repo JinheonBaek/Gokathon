44373f4579daf38e438caa0c09bcf9d0376f1bcd
Wonjong Oh <Wonjong Oh>
2017-08-05 11:43:56 +0900

    Add webcam function

---
+++ js/snapshot.js
@@ -0,0 +1,38 @@
+// webcamSnapshot
+// Webcam snapshot library using webcam.js in JavaScript 
+
+//Taking snapshot
+function snapshot(data) {
+	Webcam.snap(
+		function(data_uri) {
+			var raw_image_data = data.replace(/^data\:image\/\w+\;base64\,/, '');
+			var time = new Date();
+			var usrName = document.getElementById('usrname').value;
+			var imgName = usrName + time.getTime() + '.jpg';
+			
+			//Upload snapshot image on server for jpeg format
+			$(function() {
+				var formData = new FormData();
+				formData.append('mydata', raw_image_data);
+				formData.append('imgname', imgName);
+
+				$.ajax( {
+					url: 'upload.php',
+					data: formData,
+					async: false,
+					processData: false,
+					contentType: false,
+					type: 'POST',
+					success: function(data) {
+						document.getElementById('resultsInfo').innerHTML = 'The image data is uploaded to the server';
+						alert("업로드 성공")
+					},
+					error: function(error) {
+						document.getElementById('resultsInfo').innerHTML = "Error!";
+						alert(error);
+					}
+				}); 
+			}); 				
+		}
+	);
+}
