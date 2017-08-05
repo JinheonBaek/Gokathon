a94cb714ac1f5c70f72312f4cdfb1bb903e5de08
Wonjong Oh <Wonjong Oh>
2017-08-05 15:18:16 +0900

    Add Enroll Function - first

--- js/cog.js
+++ js/cog.js
@@ -1,10 +1,14 @@
 //Cog
 
-function addFace(usrName) {
-	var faceListId = "celebrity";
-	var subscriptionKey = "39e75e2ea70c4d14b70e4c29d3d61fde";
-	//var imgPath = "http://jinheon.azurewebsites.net/EnrollFace/image/" + usrName + '.jpg';
+//Config
+var subscriptionKey = "39e75e2ea70c4d14b70e4c29d3d61fde";
 
+//Enroll Face to Face List
+function addFace() {
+	var usrName = document.getElementById('celname').value;
+	var imgPath = document.getElementById('url').value;
+	var faceListId = "celebrity";
+	
 	$(function() {
 		var params = {
 			// Request parameters
@@ -33,4 +37,67 @@ function addFace(usrName) {
 			alert("error");
 		});
 	});
+}
+
+// Get FaceID
+function getFaceId(imgName) {
+	var imgPath = "http://jinheon.azurewebsites.net/VerifyFace/image/"+ imgName;
+
+	$(function() {
+		var params = {
+			// Request parameters
+			"returnFaceId": "true",
+			"returnFaceLandmarks": "false",
+			//"returnFaceAttributes": "{string}",
+		};
+		$.ajax( {
+			url: "https://westus.api.cognitive.microsoft.com/face/v1.0/detect?" + $.param(params),
+			beforeSend: function(xhrObj){
+				// Request headers
+				xhrObj.setRequestHeader("Content-Type","application/json");
+				xhrObj.setRequestHeader("Ocp-Apim-Subscription-Key", subscriptionKey);
+			},
+			type: "POST",
+			async: false,
+			// Request body
+			data: "{" + 
+				"\"url\":\"" + imgPath + 
+			"\"}"
+		})
+		.done(function(data) {
+			var str = JSON.stringify(data);
+			faceId = str.split(":").toString().split(",");
+		})
+		.fail(function() {
+			alert("error");
+		});
+	});
+
+	return faceId[1];
+}
+
+function getFaceList() {
+    $(function() {
+        var params = {
+            // Request parameters
+        };
+      
+        $.ajax({
+            url: "https://southeastasia.api.cognitive.microsoft.com/face/v1.0/facelists/celebrity?" + $.param(params),
+            beforeSend: function(xhrObj){
+                // Request headers
+                xhrObj.setRequestHeader("Ocp-Apim-Subscription-Key", subscriptionKey);
+            },
+            type: "GET",
+            // Request body
+            data: "{body}",
+        })
+        .done(function(data) {
+			alert("success");
+			return data;
+        })
+        .fail(function() {
+            alert("error");
+        });
+    });
 }
